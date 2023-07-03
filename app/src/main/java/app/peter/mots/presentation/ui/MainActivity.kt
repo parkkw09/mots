7fbf164e3b028a1f21b9cff5679cc3f9c4d8f548
parkkw09-iMac <parkkw09@gmail.com>
2023-07-03 21:50:44 +0900

    add ktor, seoul url

--- app/src/main/java/app/peter/mots/presentation/ui/MainActivity.kt
+++ app/src/main/java/app/peter/mots/presentation/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package app.peter.mots.presentation.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,9 +9,48 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import app.peter.mots.R
 import app.peter.mots.presentation.ui.theme.MidstOfTheSeoulTheme
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.serialization.gson.gson
+import kotlinx.coroutines.launch
+
+const val TAG = "MOTS"
+class Greeting {
+    private val client = HttpClient(CIO) {
+        install(Logging) {
+            logger = object: Logger {
+                override fun log(message: String) {
+                    Log.d(TAG, message)
+                }
+            }
+            level = LogLevel.ALL
+        }
+        install(ContentNegotiation) {
+            gson()
+        }
+    }
+
+    suspend fun greeting(key: String): String {
+        val response = client.get("http://openapi.seoul.go.kr:8088/${key}/json/culturalEventInfo/1/5")
+        return response.bodyAsText()
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +62,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val key = getString(R.string.seoul_key)
+                    Greeting(key)
                 }
             }
         }
@@ -31,13 +72,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val scope = rememberCoroutineScope()
+    var text by remember { mutableStateOf("Loading") }
+    LaunchedEffect(true) {
+        scope.launch {
+            text = try {
+                Greeting().greeting(name)
+            } catch (e: Exception) {
+                e.localizedMessage ?: "error"
+            }
+        }
+    }
     Text(
-        text = "Hello $name!",
+        text = text,
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MidstOfTheSeoulTheme {
